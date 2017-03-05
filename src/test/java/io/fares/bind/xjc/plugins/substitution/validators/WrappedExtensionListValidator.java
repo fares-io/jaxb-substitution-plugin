9a78e96738b2fe70d0b6a4bb61476fcd7701c26c
Niels Bertram <niels.bertram@fares.io>
2017-03-05 20:59:05 +1000

    Updated for first release * added Apache 2 license * updated groupId to ensure consistent names domain * added sonatype plugins and updated all other plugins to be able to release to maven central * updated usage instructions

--- src/test/java/io/fares/bind/xjc/plugins/substitution/validators/WrappedExtensionListValidator.java
+++ src/test/java/io/fares/bind/xjc/plugins/substitution/validators/WrappedExtensionListValidator.java
@@ -1,18 +1,36 @@
-package io.fares.jaxb.xjc.plugins.substitution.validators;
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package io.fares.bind.xjc.plugins.substitution.validators;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.ReferenceType;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import javax.xml.bind.annotation.XmlElementRef;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class ExtensionListValidator extends TestValidator {
+public class WrappedExtensionListValidator extends TestValidator {
 
 
   @Override
@@ -22,7 +40,7 @@ public class ExtensionListValidator extends TestValidator {
     Node parent = n.getParentNode();
 
     if (parent instanceof TypeDeclaration &&
-      "Context".equals(((TypeDeclaration) parent).getName()) &&
+      "Extensions".equals(((TypeDeclaration) parent).getName()) &&
       n.getVariables().size() == 1 && "extension".equals(n.getVariables().get(0).getId().getName())) {
 
       this.found = true;
