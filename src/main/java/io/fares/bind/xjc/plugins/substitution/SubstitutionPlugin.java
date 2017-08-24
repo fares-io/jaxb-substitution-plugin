cd6d7ab7cc11c0a46b53385f150805230e91c697
Niels Bertram <nielsbne@gmail.com>
2017-08-24 14:01:30 +1000

    refactored processing the class model info to avoid ConcurrentModificationException

--- src/main/java/io/fares/bind/xjc/plugins/substitution/SubstitutionPlugin.java
+++ src/main/java/io/fares/bind/xjc/plugins/substitution/SubstitutionPlugin.java
@@ -19,8 +19,10 @@
  */
 package io.fares.bind.xjc.plugins.substitution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementRef;
@@ -42,7 +44,6 @@ import com.sun.tools.xjc.outline.ClassOutline;
 import com.sun.tools.xjc.outline.FieldOutline;
 
 import com.sun.tools.xjc.model.*;
-import com.sun.tools.xjc.model.CElementPropertyInfo.CollectionMode;
 
 import org.jvnet.jaxb2_commons.plugin.AbstractParameterizablePlugin;
 
@@ -106,124 +107,19 @@ public class SubstitutionPlugin extends AbstractParameterizablePlugin {
 
     });
 
-
+    // need to do this as there is another call to classInfo.getProperties() inside the visitor that
+    // otherwise causes a ConcurrentModificationException
+    List<CPropertyInfo> cpy = new ArrayList<>(50);
     for (CPropertyInfo property : classInfo.getProperties()) {
-
-      property.accept(new CPropertyVisitor<Void>() {
-
-        public Void onElement(CElementPropertyInfo element) {
-
-          boolean isCandidate = Customisation.hasCustomizationsInProperty(element, SUBSTITUTION_HEAD_REF_NAME);
-
-          for (CTypeInfo ref : element.ref()) {
-            isCandidate = isCandidate || Customisation.hasCustomizationsInType(ref, SUBSTITUTION_HEAD_NAME);
-          }
-
-          if (isCandidate) {
-            element.getAdapter();
-          }
-
-          return null;
-        }
-
-        public Void onAttribute(CAttributePropertyInfo attribute) {
-          return null;
-        }
-
-        public Void onValue(CValuePropertyInfo property) {
-          return null;
-        }
-
-        public Void onReference(final CReferencePropertyInfo property) {
-
-          // TODO check if the property, accessor or referenced class has a substitution annotation
-
-          boolean hasPropertyCustomizations = Customisation.hasCustomizationsInProperty(property, SUBSTITUTION_HEAD_REF_NAME);
-
-          boolean hasTargetElementCustomizations = false;
-
-          // REVIEW should check if there is only 1 reference
-          for (CElement element : property.getElements()) {
-            hasTargetElementCustomizations = hasTargetElementCustomizations || Customisation.hasCustomizationsInType(element, SUBSTITUTION_HEAD_NAME);
-          }
-
-          if (hasPropertyCustomizations || hasTargetElementCustomizations) {
-
-            int index = classInfo.getProperties().indexOf(property);
-
-            for (CElement element : property.getElements()) {
-
-              final CElementPropertyInfo elementPropertyInfo;
-
-              if (element instanceof CElementInfo) {
-                // FIXME need to translate the element type reference head customizations into head-ref ones
-                CCustomizations customizations = hasPropertyCustomizations ? property.getCustomizations() : element.getCustomizations();
-                elementPropertyInfo = createElementPropertyInfo(property, (CElementInfo) element, customizations);
-              } else if (element instanceof CClassInfo) {
-                elementPropertyInfo = null;
-              } else if (element instanceof CClassRef) {
-                elementPropertyInfo = null;
-              } else {
-                elementPropertyInfo = null;
-              }
-
-              if (elementPropertyInfo != null) {
-                // hah this is fun, setParent is package scoped on CPropertyInfo so the only way to get
-                // the parent set on the property is to first add it using the CClassInfo.addProperty
-                // method, then remove it from the collection and then add it back with the correct index
-                classInfo.addProperty(elementPropertyInfo);
-                classInfo.getProperties().remove(elementPropertyInfo);
-                classInfo.getProperties().add(index++, elementPropertyInfo);
-                classInfo.getProperties().remove(property);
-              }
-            }
-          }
-
-          return null;
-
-        }
-
-      });
-
-    }
-  }
-
-  private CElementPropertyInfo createElementPropertyInfo(final CReferencePropertyInfo property,
-                                                         final CElementInfo elementInfo,
-                                                         final CCustomizations customizations) {
-
-    final CElementPropertyInfo elementPropertyInfo = new CElementPropertyInfo(
-      property.getName(false),
-      property.isCollection() ? CollectionMode.REPEATED_ELEMENT : CollectionMode.NOT_REPEATED,
-      property.id(),
-      property.getExpectedMimeType(),
-      property.getSchemaComponent(),
-      customizations,
-      property.getLocator(),
-      property.isRequired());
-
-    // also need to set the public name
-    elementPropertyInfo.setName(true, property.getName(true));
-
-    // add adapters of the ref type to this element as it is not going to be referenced anymore
-    final CAdapter adapter = elementInfo.getProperty().getAdapter();
-
-    if (adapter != null) {
-      elementPropertyInfo.setAdapter(adapter);
+      cpy.add(property);
     }
 
-    // add the type reference to the field
-    CTypeRef ref = new CTypeRef(
-      elementInfo.getContentType(),
-      elementInfo.getElementName(),
-      elementInfo.getContentType().getTypeName(),
-      false,
-      null);
+    for (CPropertyInfo property : cpy) {
+      property.accept(new SubstitutionPropertyVisitor(classInfo));
+    }
 
-    elementPropertyInfo.getTypes().add(ref);
-
-    return elementPropertyInfo;
   }
+
 
   @Override
   public boolean run(Outline outline, Options opt, ErrorHandler errorHandler) throws SAXException {
